8b8a753ff9707c7913883e44690f621f30d77bae
Jérémiah <jeremiah-haulin@etud.univ-tln.fr>
2018-01-24 17:54:43 +0100

    astuces et conseils

--- site/actuces.php
+++ site/actuces.php
@@ -2,10 +2,10 @@
 <html>
 <head>
 		<meta charset="utf-8">
-		<title>RPG</title>
-		<link href="css/style.css" rel="stylesheet" type="text/css">
-		<link rel="icon" type="image/png" href="img/favicon.png" />
-		<script src="js/burger.js"></script>
+		<title>RPG - Règles du Jeu</title>
+		<link href="../css/style.css" rel="stylesheet" type="text/css">
+		<link rel="icon" type="image/png" href="../img/favicon.png" />
+		<script src="../js/burger.js"></script>
 	</head>
 
 	<body>
@@ -13,14 +13,13 @@
              <div id="conteneur_menu">
 				 <div id="leburger">
 					 <div class="burger">
-						 <img src="./img/burger_icon.png" id="menu_burger"/>
+						 <img src="../img/burger_icon.png" id="menu_burger"/>
 					 </div>
 				 </div>
 				 <div id="lemenu">
 					 <div class="menu">
 						 <ul>
-							<a href="index.php" class="liens_menu"><li>FALLEN IN HELL</li></a>
-							<!--<a href="site/regles.php" class="liens_menu"><li>RÈGLES DU JEU</li></a>-->
+							<a href="../index.php" class="liens_menu"><li id="titre_liste">FALLEN IN HELL</li></a>
 						 </ul>
 					 </div>
 				 </div>
@@ -29,15 +28,9 @@
 		<div id="partie_corps">
 			<div id="conteneur_corps">
 				<div id="structure_corps">
-					<div class="content_corps"> 
-						<a href="site/regles.php" class="liens_menu"><li>REGLES DU JEU</li></a>
-						<a href="site/actuces.php" class="liens_menu"><li>Astuces et conseils</li></a>
-						<!--<h1>RPG</h1>
+					<div class="content_corps">
+						<h1>Astuces et Conseils</h1>
 						<br>
-						<?php
-							//include ('./php/jeu.php');
-						?>-->
-
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam dignissim turpis nec erat interdum, non euismod quam ultrices. Ut ultrices facilisis augue. Suspendisse vel commodo dolor. Cras nisi dolor, tempus vitae metus vitae, efficitur fermentum diam. Sed mollis gravida tellus, nec lacinia augue. Sed sodales et nisi id aliquam. Phasellus laoreet nunc eu sagittis mattis. Integer commodo sollicitudin tellus. Sed iaculis sem est, consectetur dictum diam condimentum sit amet. Quisque non velit cursus, consequat dui venenatis, pulvinar lectus.
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam dignissim turpis nec erat interdum, non euismod quam ultrices. Ut ultrices facilisis augue. Suspendisse vel commodo dolor. Cras nisi dolor, tempus vitae metus vitae, efficitur fermentum diam. Sed mollis gravida tellus, nec lacinia augue. Sed sodales et nisi id aliquam. Phasellus laoreet nunc eu sagittis mattis. Integer commodo sollicitudin tellus. Sed iaculis sem est, consectetur dictum diam condimentum sit amet. Quisque non velit cursus, consequat dui venenatis, pulvinar lectus.
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam dignissim turpis nec erat interdum, non euismod quam ultrices. Ut ultrices facilisis augue. Suspendisse vel commodo dolor. Cras nisi dolor, tempus vitae metus vitae, efficitur fermentum diam. Sed mollis gravida tellus, nec lacinia augue. Sed sodales et nisi id aliquam. Phasellus laoreet nunc eu sagittis mattis. Integer commodo sollicitudin tellus. Sed iaculis sem est, consectetur dictum diam condimentum sit amet. Quisque non velit cursus, consequat dui venenatis, pulvinar lectus.
@@ -48,7 +41,7 @@
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam dignissim turpis nec erat interdum, non euismod quam ultrices. Ut ultrices facilisis augue. Suspendisse vel commodo dolor. Cras nisi dolor, tempus vitae metus vitae, efficitur fermentum diam. Sed mollis gravida tellus, nec lacinia augue. Sed sodales et nisi id aliquam. Phasellus laoreet nunc eu sagittis mattis. Integer commodo sollicitudin tellus. Sed iaculis sem est, consectetur dictum diam condimentum sit amet. Quisque non velit cursus, consequat dui venenatis, pulvinar lectus.
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam dignissim turpis nec erat interdum, non euismod quam ultrices. Ut ultrices facilisis augue. Suspendisse vel commodo dolor. Cras nisi dolor, tempus vitae metus vitae, efficitur fermentum diam. Sed mollis gravida tellus, nec lacinia augue. Sed sodales et nisi id aliquam. Phasellus laoreet nunc eu sagittis mattis. Integer commodo sollicitudin tellus. Sed iaculis sem est, consectetur dictum diam condimentum sit amet. Quisque non velit cursus, consequat dui venenatis, pulvinar lectus.
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam dignissim turpis nec erat interdum, non euismod quam ultrices. Ut ultrices facilisis augue. Suspendisse vel commodo dolor. Cras nisi dolor, tempus vitae metus vitae, efficitur fermentum diam. Sed mollis gravida tellus, nec lacinia augue. Sed sodales et nisi id aliquam. Phasellus laoreet nunc eu sagittis mattis. Integer commodo sollicitudin tellus. Sed iaculis sem est, consectetur dictum diam condimentum sit amet. Quisque non velit cursus, consequat dui venenatis, pulvinar lectus.
-					</div> 
+					</div>
 				</div>
 			</div>
 		</div>
